4fb8eda739a51393bbb881a101416914f76472b3
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2017-09-23 16:01:20 +0900

    ホスト側セッション追加

---
+++ app/controllers/host/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Host::SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :host do
+    get 'sessions/new'
+  end
+
+  namespace :host do
+    get 'sessions/create'
+  end
+
+  namespace :host do
+    get 'sessions/destroy'
+  end
+
   root to: 'top#show'
 
   # 演奏者側
@@ -7,6 +19,14 @@ Rails.application.routes.draw do
 
     # ログイン・ログアウト
     resource :sessions, only: [:new, :create, :destroy]
+    # 予約
     resources :reservations
+  end
+
+  namespace :host do
+    root to: 'sessions#new'
+
+    # ログイン・ログアウト
+    resource :sessions, only: [:new, :create, :destroy]
   end
 end
