788ad31f80547f726c87f15475a002c71a5d223e
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2017-10-08 20:43:53 +0900

    演奏者の最初のページを専用メニュー画面に変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
     # ログイン・ログアウト
     resource :sessions, only: [:new, :create, :destroy]
+    get 'menu', to: 'menu#show'
+
     # 予約
     resources :reservations
   end
