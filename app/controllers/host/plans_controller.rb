2ac3f9da6220081c108a0ca2415c9d9a71e53a21
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2017-10-08 17:03:24 +0900

    プランコントローラ

--- app/controllers/host/plans_controller.rb
+++ app/controllers/host/plans_controller.rb
@@ -1,2 +1,26 @@
 class Host::PlansController < Host::BaseController
+  before_action :find_room, only: [:new, :create]
+  before_action :find_plan, only: [:edit, :update]
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def find_room
+    @room = Room.find_by(id: params[:room_id])
+  end
+
+  def find_plan
+    @plan = Plan.find_by(id: params[:id])
+  end
 end
