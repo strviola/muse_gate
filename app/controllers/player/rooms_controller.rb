09ccb639a2e89eeefd485d1cc73754504a641b2f
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2017-10-16 03:07:31 +0900

    練習室一覧ページ作成

--- app/controllers/player/rooms_controller.rb
+++ app/controllers/player/rooms_controller.rb
@@ -1,2 +1,9 @@
 class Player::RoomsController < ApplicationController
+  def index
+    # TODO: 絞り込み機能 (Hostをブックマーク、住所が近い、など)
+    @rooms = Room.all.includes(:host, address: :prefecture)
+  end
+
+  def show
+  end
 end
