07ffb7c6ff86b39cf9a78548d67833187e1c3ad3
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2017-10-21 15:41:26 +0900

    controller method

--- app/controllers/host/reservations_controller.rb
+++ app/controllers/host/reservations_controller.rb
@@ -1,2 +1,18 @@
-class Host::ReservationsController < ApplicationController
+class Host::ReservationsController < Host::BaseController
+  before_action :find_reservation, only: [:edit, :update]
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def find_reservation
+    @reservation = Reservation.find(params[:id])
+  end
 end
--- app/controllers/player/reservations_controller.rb
+++ app/controllers/player/reservations_controller.rb
@@ -1,4 +1,29 @@
 class Player::ReservationsController < Player::BaseController
+  before_action :find_reservation, only: [:edit, :update]
+  before_action :find_room, only: [:new, :create]
+
   def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def find_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def find_room
+    @room = Room.find(params[:room_id])
   end
 end
