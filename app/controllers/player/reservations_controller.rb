7e51634f3cc8e4116463a7879f35c7b09f51a728
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2017-10-21 18:11:21 +0900

    予約作成画面

--- app/controllers/player/reservations_controller.rb
+++ app/controllers/player/reservations_controller.rb
@@ -1,11 +1,12 @@
 class Player::ReservationsController < Player::BaseController
   before_action :find_reservation, only: [:edit, :update]
-  before_action :find_room, only: [:new, :create]
+  before_action :find_room_plan, only: [:new, :create]
 
   def index
   end
 
   def new
+    @reservation = Reservation.new(room: @room, plan: @plan, status: :initial)
   end
 
   def create
@@ -23,7 +24,8 @@ class Player::ReservationsController < Player::BaseController
     @reservation = Reservation.find(params[:id])
   end
 
-  def find_room
+  def find_room_plan
     @room = Room.find(params[:room_id])
+    @plan = Plan.find(params[:plan_id])
   end
 end
